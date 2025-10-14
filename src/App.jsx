3237e7a92c08ec9a05c8732716e375b6a3ca82cb
DevAbidHasan <abidhasanplabon80@gmail.com>
2025-10-14 14:34:46 +0600

    first commit:project setup done, font, and other packages installed

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h2 className='text-3xl font-black inter my-5 text-center'>CSE ju official website</h2>
+     <h2 className='text-3xl font-black  my-5 text-center'>CSE ju official website</h2>
+     <h2 className='text-3xl font-black poppins my-5 text-center'>CSE ju official website</h2>
+     <p className='text-blue-500 inter mx-5'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores omnis ratione velit sit dolorum tenetur pariatur aut voluptatum modi voluptatem quam facere cupiditate doloremque architecto, rem commodi eligendi in consectetur est facilis accusamus minus nostrum dolores! Praesentium cumque tempora nesciunt?</p>
+     
+    </>
+  )
+}
+
+export default App
